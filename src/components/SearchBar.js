2b4d09f8b8a4f027d99c4e7ebb82141dee4c71d4
nstickler28 <nathanstickler@gmail.com>
2022-06-16 20:29:16 -0700

    Added callback to prevent default when submit form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        event.target.value
+    // onInputChange(event) {
+    //     event.target.value
+    // }
+
+    state = { term: '' };
+
+    onFormSubmit(event) {
+        event.preventDefault();
     }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} />
+                        {/* onChange={(event) => event.target.value} */}
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={(e) => this.setState({ term: e.target.value })}
+                        />
                     </div>
                 </form>
             </div>
