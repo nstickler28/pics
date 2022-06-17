2f1fbf62706fae0c9c7f4c0d62a8ca83f9e719db
nstickler28 <nathanstickler@gmail.com>
2022-06-16 21:58:19 -0700

    Made App component into a class

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,9 +7,10 @@ class SearchBar extends React.Component {
 
     state = { term: '' };
 
-    onFormSubmit(event) {
+    onFormSubmit = event => {
         event.preventDefault();
-    }
+        this.props.onSubmit(this.state.term);
+    };
 
     render() {
         return (
