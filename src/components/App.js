4178e740c85c20c6f03b583e337f859a828ad041
nstickler28 <nathanstickler@gmail.com>
2022-06-17 19:54:25 -0700

    Set state after fetching request and bind callback

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,23 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID JFqYfzaH4mTr3GilpPPliNGHpeyELJ4GGOUv_RgwNig'
             }
         });
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
